ee37321972fb796a8cde833985ed28efb58a7bdb
buffruan <44060668+buffruan@users.noreply.github.com>
2021-01-02 11:16:39 +0800

    mainController

---
+++ src/main/java/web/weiboer/controller/mainController.java
@@ -0,0 +1,14 @@
+package web.weiboer.controller;
+import web.weiboer.dataStruct.weiboerContent;
+import web.weiboer.dataStruct.weiboerUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.concurrent.Callable;
+public class mainController {
+}
