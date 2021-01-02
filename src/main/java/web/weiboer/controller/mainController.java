01d8239f33973ea7867e8b3d2348231994f56656
buffruan <44060668+buffruan@users.noreply.github.com>
2021-01-02 14:05:22 +0800

    mainController

--- src/main/java/web/weiboer/controller/mainController.java
+++ src/main/java/web/weiboer/controller/mainController.java
@@ -1,4 +1,9 @@
 package web.weiboer.controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import web.weiboer.dao.ContentRepository;
 import web.weiboer.dao.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,4 +16,22 @@ public class mainController {
     @Autowired
     private ContentRepository contentRepository;
 
+    @GetMapping("/login")
+    public String login(){
+        return "main";
+    }
+
+    @RequestMapping("/main")
+    public ModelAndView index(Model model,
+                              @RequestParam(value = "page",defaultValue = "1")Integer page){
+        ModelAndView mav = new ModelAndView();
+        return mav;
+    }
+
+    @RequestMapping("/my")
+    public ModelAndView my(Model model,
+                           @RequestParam(value = "username")String username){
+        ModelAndView mav = new ModelAndView();
+        return mav;
+    }
 }
